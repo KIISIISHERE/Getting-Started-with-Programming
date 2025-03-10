fbbb2c302c2eb12270c86e47ef81b3a57ecf8b79
KIISIISHERE <iteoluwakiisi420@gmail.com>
2025-03-10 17:50:45 +0000

    I made a code

---
+++ Main.py
@@ -0,0 +1,15 @@
+#exploring the print statement
+#printing a word
+print("Hello")
+#printing a sentence
+print("Hello world, I am getting ready with Programming")
+#printing a nu,ber
+print(12)
+#printing a decimal number
+print(12.5)
+#printing a sentence then a blank line
+print("Hello world \n")
+#print a sentence together with a number
+print("Hello world I am getting ready with Programming", 12)
+#add a* at the end of a sentence
+print("Hello world,", end="*")
---
+++ Main2.py
@@ -0,0 +1,7 @@
+#create a variable
+name="Iteoluwakiisi"
+Grade=7
+school="Chrisland High School Ikeja"
+country="Nigeria"
+#print out the value of the variable in one sentence
+print("My name is ",name,"and I am in grade ",Grade,". I go to ",school,"school and I come from ",country)
